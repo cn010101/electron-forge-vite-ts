5a65ac1cc951d90c0ad0706af47f7d2ab4c1fc6a
small earth <151430028+cn010101@users.noreply.github.com>
2024-01-07 09:35:55 +0800

    简单的 Tray 实例

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
+import { app, BrowserWindow, Tray, nativeImage, Menu } from "electron";
+import path from "path";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
@@ -12,7 +12,7 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
@@ -20,7 +20,9 @@ const createWindow = () => {
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
   }
 
   // Open the DevTools.
@@ -30,18 +32,39 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+// app.on("ready", createWindow);
+let tray;
+app.whenReady().then(() => {
+  createWindow();
+  // icon 的目录是编译后的资源目录的文件目录(./.vite/build),
+  // 获取目录要采用 path.join(__dirname, "assets/icons/icon.ico")
+  // 文件类型 可以是 png | ico | .....
+  const icon = nativeImage.createFromPath(
+    path.join(__dirname, "assets/icons/icon.ico")
+  );
+  tray = new Tray(icon);
 
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Item1", type: "radio" },
+    { label: "Item2", type: "radio" },
+    { label: "Item3", type: "radio", checked: true },
+    { label: "Item4", type: "radio" },
+  ]);
+
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip("This is my application");
+  tray.setTitle("This is my title");
+});
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from "vite";
+import copy from "rollup-plugin-copy";
 // https://vitejs.dev/config
 export default defineConfig({
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
-    mainFields: ['module', 'jsnext:main', 'jsnext'],
+    mainFields: ["module", "jsnext:main", "jsnext"],
   },
+  plugins: [
+    copy({
+      targets: [
+        { src: "./src/assets/icons/*", dest: "./.vite/build/assets/icons" },
+      ],
+    }),
+  ],
 });
